36e3ea2735f056356fb8346b8930c6c2fc1c94cb
Maria <mariasuslova1337@gmail.com>
2024-07-07 14:28:10 +0400

    scroll pane (no data)

--- script.js
+++ script.js
@@ -1,124 +1,80 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const itemsContainer = document.getElementById('imageContainer');
-    let totalItems = 0;
-    let currentIndex = 0;
-    let headerIndexes = {}; // Объявляем здесь, чтобы была видна во всех функциях
-    let slides = []; // Объявляем переменную slides
-
-    async function fetchItems() {
-        showLoader();
-        console.log('Fetching items from Google Sheets...');
-        try {
-            const response = await fetch('https://script.google.com/macros/s/AKfycbwt3hiNq0oU2zywpaLlpy-ILCZljRw4hwTjFjEhzx_iPZkp1jZKRqXTHmd-LCU0f5t9vA/exec');
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            const data = await response.json();
-            console.log('Data fetched from Google Sheets:', data);
-
-            // Установка headerIndexes
-            const headers = data[0];
-            headers.forEach((header, index) => {
-                headerIndexes[header.toLowerCase()] = index;
+$(document).ready(function() {
+            Init();
+        
+            // Mouse Wheel event : jQuery Mouse Wheel Plugin
+            $('.pane,.scrzone').mousewheel(function(event) {
+                event.preventDefault();
+                if($ScrollState == false) {
+                    $ScrollState = true;
+                    if(event.deltaY < 0) {
+                        UpdateScreen('+');
+                    } else if(event.deltaY > 0) {
+                        UpdateScreen('-');
+                    } else {
+                        $ScrollState = false;
+                    }
+                }
             });
-
-            // Инициализация slides
-            slides = data.slice(1); // Пример: используем данные без заголовков
-
-            return slides || [];
-        } catch (error) {
-            console.error('Error fetching items:', error);
-        } finally {
-            hideLoader();
-        }
-    }
-
-    function displayItems(items) {
-        items.forEach((item, index) => {
-            const name = item[headerIndexes['name']];
-            const price = item[headerIndexes['price']];
-            const url = item[headerIndexes['url']];
-
-            const itemElement = document.createElement('div');
-            itemElement.classList.add('item');
-
-            const img = document.createElement('img');
-            img.src = url;
-
-            const nameElement = document.createElement('p');
-            nameElement.textContent = name;
-            nameElement.classList.add('item-name');
-
-            const priceElement = document.createElement('p');
-            priceElement.textContent = `${price} GEL`;
-            priceElement.classList.add('item-price');
-
-            itemElement.appendChild(img);
-            itemElement.appendChild(nameElement);
-            itemElement.appendChild(priceElement);
-            itemsContainer.appendChild(itemElement);
-
-            // Создание слайда для вертикального скроллинга
-            const slideElement = document.createElement('div');
-            slideElement.classList.add('scr');
-            slideElement.textContent = name; // Используйте имя товара или другие данные для отображения
-            slideElement.setAttribute('data-id', `slide${index + 1}`);
-            slideElement.addEventListener('click', () => {
-                currentIndex = index;
-                showItem(currentIndex);
-            });
-            scrollPane.appendChild(slideElement);
-        });
-
-        // Установка общего количества элементов
-        totalItems = items.length;
-    }
-
-    function convertToDirectLink(url) {
-        // Add your logic to convert the URL to a direct link if necessary
-        return url;
-    }
-
-    function showLoader() {
-        const loaderContainer = document.getElementById('loaderContainer');
-        loaderContainer.style.display = 'flex';
-    }
-
-    function hideLoader() {
-        const loaderContainer = document.getElementById('loaderContainer');
-        loaderContainer.style.display = 'none';
-    }
-
-    function showItem(index) {
-        const items = document.querySelectorAll('.item');
-        items.forEach((item, idx) => {
-            if (idx === index) {
-                item.classList.add('active');
-            } else {
-                item.classList.remove('active');
+        
+            // Init
+            function Init() {
+                $ScrollSpeed = 0.3; // Animation speed
+                $ScrollState = false; // Scroll possible if True - If False anim already in progress
+                $ActualSlide = $CibleSlide = $('.pane').first().attr('data-id'); // First slide
+                $ListSlides = new Array();
+                $('.pane').each(function() {
+                    $ListSlides.push($(this).attr('data-id'));
+                }); // List of slides (.pane)
+                gsap.to(window, { duration: 0, scrollTo: { y: 0 } });
+                gsap.to('.spane', { duration: 0, scrollTo: { y: 0, x: 0 } });
+                $('.visible').removeClass('visible');
+                $('#Helper').html("Init()"); // Helper
             }
+        
+            // ANIMATE
+            function UpdateScreen(operator) {
+                $ActualSlide = $CibleSlide;
+                if(operator == "+") {
+                    $CibleSlide = $ListSlides[$ListSlides.indexOf($ActualSlide) + 1];
+                } else {
+                    $CibleSlide = $ListSlides[$ListSlides.indexOf($ActualSlide) - 1];
+                } // If + next slide / if - previous slide
+                $('#Helper').html("From <strong>" + $ActualSlide + "</strong> to <strong>" + $CibleSlide + "</strong>"); // helper
+                if(!$CibleSlide) {
+                    $ScrollState = false;
+                    $('#Helper').html("Break");
+                    $CibleSlide = $ActualSlide;
+                    return;
+                } // Stops everything if there is no slide before/after
+                $ActualSlideDOM = $('.pane[data-id=' + $ActualSlide + ']');
+                $CibleSlideDOM = $('.pane[data-id=' + $CibleSlide + ']');
+                // Scroll To : Greensock GSAP
+                if($ActualSlideDOM.closest('.prt').find('.spane').length && 
+                   (operator == "+" && $ActualSlideDOM.next('.pane').length || operator == "-" && $ActualSlideDOM.prev('.pane').length)) {
+                    gsap.to($ActualSlideDOM.closest('.spane'), {
+                        duration: $ScrollSpeed,
+                        scrollTo: { y: $CibleSlideDOM.position().top },
+                        ease: "power2.out",
+                        onComplete: function() {
+                            $ScrollState = false;
+                            $CibleSlideDOM.addClass('visible');
+                        }
+                    });
+                } else {
+                    gsap.to(window, {
+                        duration: $ScrollSpeed,
+                        scrollTo: { y: $CibleSlideDOM.offset().top },
+                        ease: "power2.out",
+                        onComplete: function() {
+                            $ScrollState = false;
+                            $CibleSlideDOM.addClass('visible');
+                        }
+                    });
+                }
+            }
+        
+            // Init() On Resize
+            $(window).resize(function() {
+                Init();
+            });
         });
-    }
-
-    fetchItems().then(items => {
-        if (items && items.length > 0) {
-            displayItems(items);
-        } else {
-            console.log('No items found.');
-        }
-    });
-
-    // Добавление инициализации скролла для вертикального скролла слайдов
-    const scrollPane = document.getElementById('ScrollPane');
-
-    // Обработка события скроллинга для переключения между слайдами
-    window.addEventListener('wheel', function(event) {
-        event.preventDefault();
-        if (event.deltaY < 0) {
-            currentIndex = Math.max(0, currentIndex - 1);
-        } else {
-            currentIndex = Math.min(slides.length - 1, currentIndex + 1);
-        }
-        showItem(currentIndex);
-    }, { passive: false });
-});
